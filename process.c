56d04b7ccdd5d9b7d759c3fd17ce9eb0876d2d73
Cullen Braeden <bc832@zoo.cs.yale.edu>
2024-10-30 10:08:37 -0400

    basic outline

--- process.c
+++ process.c
@@ -1,79 +1,182 @@
+// process.c
 #include "process.h"
+#include "parse.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 
+// Function to execute simple commands
+int execute_simple(const CMD *cmd) {
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return errno;
+    } else if (pid == 0) {
+        // Child process
+
+        // Handle I/O Redirection
+        if (cmd->fromType != NONE) {
+            int fd;
+            if (cmd->fromType == RED_IN) {
+                fd = open(cmd->fromFile, O_RDONLY);
+                if (fd < 0) {
+                    perror("open");
+                    exit(errno);
+                }
+                dup2(fd, STDIN_FILENO);
+                close(fd);
+            }
+            // Handle HERE documents (RED_IN_HERE)
+            // Implementation needed
+        }
+
+        if (cmd->toType != NONE) {
+            int fd;
+            if (cmd->toType == RED_OUT) {
+                fd = open(cmd->toFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            } else if (cmd->toType == RED_OUT_APP) {
+                fd = open(cmd->toFile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+            } else { // RED_OUT_ERR (&>)
+                fd = open(cmd->toFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                // Redirect stderr as well if needed
+            }
+
+            if (fd < 0) {
+                perror("open");
+                exit(errno);
+            }
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+        }
+
+        // Handle Local Variables
+        for (int i = 0; i < cmd->nLocal; i++) {
+            setenv(cmd->locVar[i], cmd->locVal[i], 1);
+        }
+
+        // Execute the command
+        execvp(cmd->argv[0], cmd->argv);
+        // If execvp returns, an error occurred
+        perror("execvp");
+        exit(errno);
+    } else {
+        // Parent process
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            perror("waitpid");
+            return errno;
+        }
+        if (WIFEXITED(status)) {
+            return WEXITSTATUS(status);
+        } else {
+            return 128 + WTERMSIG(status);
+        }
+    }
+}
+
+// Recursive process function
 int process(const CMD *cmd) {
-	if (cmd == NULL) {
-		return 0;
-	}
-	if (cmd->type == SIMPLE) {
-		pid_t pid = fork();
-		// check for input redirection
-		if (cmd->fromType != NONE) {
-			// Input redirection
-			int fd_in;
-			if (cmd->fromType == RED_IN) {
-				fd_in = open(cmd->fromFile, O_RDONLY);
-			} else {
-				// Handle other input redirection types if needed
-			}
-			if (fd_in < 0) {
-				perror("open");
-				exit(errno);
-			}
-			if (dup2(fd_in, STDIN_FILENO) < 0) {
-				perror("dup2");
-				exit(errno);
-			}
-			close(fd_in);
-		}
-		// check for output redirection
-		if (cmd->toType != NONE) {
-			// Output redirection
-			int fd_out;
-			if (cmd->toType == RED_OUT) {
-				fd_out = open(cmd->toFile, O_WRONLY | O_CREAT | O_TRUNC, 0666);
-			} else if (cmd->toType == RED_OUT_APP) {
-				fd_out = open(cmd->toFile, O_WRONLY | O_CREAT | O_APPEND, 0666);
-			} else {
-				// Handle other output redirection types if needed
-			}
-			if (fd_out < 0) {
-				perror("open");
-				exit(errno);
-			}
-			if (dup2(fd_out, STDOUT_FILENO) < 0) {
-				perror("dup2");
-				exit(errno);
-			}
-			close(fd_out);
-		}
-		if (pid < 0) {
-			perror("fork");
-			return errno;
-		}
-		else if (pid == 0) {
-			execvp(cmd->argv[0], cmd->argv);
-			perror("execvp");
-			exit(errno);
-		}
-		else {
-			int status;
-			if (waitpid(pid, &status, 0) < 0) {
-				perror("waitpid");
-				return errno;
-			}
-			int exit_status = WEXITSTATUS(status);
+    if (cmd == NULL) {
+        return 0;
+    }
 
-			char status_str[10];
-			snprintf(status_str, sizeof(status_str), "%d", exit_status);
+    switch (cmd->type) {
+        case SIMPLE:
+            return execute_simple(cmd);
 
-			if (setenv("?", status_str, 1) < 0) {
-				perror("setenv");
-			}
+        case PIPE: {
+            int pipe_fd[2];
+            if (pipe(pipe_fd) == -1) {
+                perror("pipe");
+                return errno;
+            }
 
-			return exit_status;
-		}
-	}
-	printf("Unsupported command type: %d\n", cmd->type);
-	return 0;
+            pid_t left_pid = fork();
+            if (left_pid < 0) {
+                perror("fork");
+                return errno;
+            }
+
+            if (left_pid == 0) {
+                // Left child
+                dup2(pipe_fd[1], STDOUT_FILENO);
+                close(pipe_fd[0]);
+                close(pipe_fd[1]);
+                exit(process(cmd->left));
+            }
+
+            pid_t right_pid = fork();
+            if (right_pid < 0) {
+                perror("fork");
+                return errno;
+            }
+
+            if (right_pid == 0) {
+                // Right child
+                dup2(pipe_fd[0], STDIN_FILENO);
+                close(pipe_fd[0]);
+                close(pipe_fd[1]);
+                exit(process(cmd->right));
+            }
+
+            // Parent process
+            close(pipe_fd[0]);
+            close(pipe_fd[1]);
+
+            int status;
+            waitpid(left_pid, &status, 0);
+            int left_status = 0;
+            if (WIFEXITED(status)) {
+                left_status = WEXITSTATUS(status);
+            } else {
+                left_status = 128 + WTERMSIG(status);
+            }
+
+            waitpid(right_pid, &status, 0);
+            int right_status = 0;
+            if (WIFEXITED(status)) {
+                right_status = WEXITSTATUS(status);
+            } else {
+                right_status = 128 + WTERMSIG(status);
+            }
+
+            // For PIPE, return the rightmost command's status
+            return right_status;
+        }
+
+        case SEP_AND: {
+            int left_status = process(cmd->left);
+            if (left_status == 0) {
+                return process(cmd->right);
+            }
+            return left_status;
+        }
+
+        case SEP_OR: {
+            int left_status = process(cmd->left);
+            if (left_status != 0) {
+                return process(cmd->right);
+            }
+            return left_status;
+        }
+
+        case SEP_END:
+        case SEP_BG:
+            // Handle sequencing and background execution
+            // Implementation needed
+            return 0;
+
+        case SUBCMD:
+            // Handle subcommands by forking a subshell
+            // Implementation needed
+            return 0;
+
+        default:
+            // Handle NONE or other types
+            return 0;
+    }
 }
